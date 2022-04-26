7f63b310ca6cfe4ce12e494929051d4cfc5c67ca
mehrsapo <mehrsa.pourya@epfl.ch>
2022-04-26 11:24:42 +0200

    initial commit

---
+++ Miniproject_1/model.py
@@ -0,0 +1,21 @@
+import torch 
+
+class Model():
+
+    def __init__(self) -> None: 
+
+        pass
+
+    def load_pretrained_model(self) -> None: 
+
+        pass
+
+    def train(self, train_input, train_target, num_epochs) -> None: 
+
+        pass
+
+    def predict(self, test_input) -> torch.Tensor: 
+
+        pass
+
+    
